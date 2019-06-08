570e91b1081839df0609c57091a8840e7524b1cb
Anubhav Ashish <anubhav.ashish9@gmail.com>
2019-06-08 19:42:08 +1000

    now added longest response times in conversation

--- chat.js
+++ chat.js
@@ -1,26 +1,63 @@
 let fs = require("fs")
-let textByLine = fs.readFileSync('_chat.txt').toString().split("\n");
+let textByLine = fs.readFileSync('_chat 2.txt').toString().split("\n");
 let regex = /\[(\d+\/\d+\/\d+),\s+(\d+:\d+:\d+\s+[a|p]m)\]\s(\w.+):\s(.+)/;
+split = null; 
+let participants = ["Kanu Tandon", "Brindha"];
 
+let DATE = 1; 
+let NAME = 3; 
+let MESSAGE  = 4; 
 
 function splitEachLine(){
-    let split = textByLine.map(elem =>{
+    split = textByLine.map(elem =>{
         return elem.match(regex);
     })
     // for every element in the array add a JS date object 
     split = split.map(elem =>{
+        let date; 
         elem[1] = function(){
-            let date = elem[1].split("/");
-            return new Date(date[0] + " "+getMonth(date[1]) +" "+ date[2] + " "+ elem[2]); 
+            if(elem != null){
+                date = elem[1].split("/");
+                return new Date(date[0] + " "+getMonth(date[1]) +" "+ date[2] + " "+ elem[2]); 
+            }
+             
+            
         }(); 
         return elem; 
     })
-    console.log(Math.abs(split[0][1] - split[10][1]));
+}
+// get each line ready
+splitEachLine(); 
+getLongestResponseTime();
+function getLongestResponseTime(){
+    let largest = 0; 
+    let conv; 
+    for(let i = 0; i < split.length; i++){
+        if(split[i][NAME] === participants[0]){
+            let responseTime = getResponseTime(split[i], getNextConversation(i));
+            if(responseTime > largest){
+                largest = responseTime; 
+                conv = split[i];
+            }    
+        }
+    }
+    console.log(conv + " \n largest response time = "+largest );
+}
+function getNextConversation(idx){
+    for(let i = idx; i < split.length; i++){
+        if(split[i][NAME] === participants[1]){
+            return split[i];
+        }
+    }
 }
 
-splitEachLine(); 
-
-
+//given two objects calculate response time 
+function getResponseTime(converationOne, conversationTwo){
+    if(conversationTwo != undefined){
+        return Math.abs(converationOne[DATE] - conversationTwo[DATE]);
+    }
+    return; 
+}
 
 
 function getMonth(date){
