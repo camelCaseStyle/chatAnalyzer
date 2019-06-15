d9c5c26c5b39bbdd528e135c29c0ad133b4592a2
Anubhav Ashish <anubhav.ashish9@gmail.com>
2019-06-15 13:44:46 +1000

    added: constants file to clean up logic

---
+++ Constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+    MONTHS : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    IGNORE_KEYWORDS : ["in","is" ,"for", "of", "a","and", "to", "are", "I", "you", "!", "image", "the", "omitted", "image", "video"],
+    REGEX : new RegExp(/\[(\d+\/\d+\/\d+),\s+(\d+:\d+:\d+\s+[a|p]m)\]\s([a-zA-Z'-\s]+):\s(.+)/),
+    DATE : 1,
+    NAME : 3, 
+    MESSAGE : 4,
+}
